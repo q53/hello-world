fcac1bf5854faaa561454f4cfcca396715857223
root <root@unsen.q53.spb.ru>
2022-05-20 16:38:50 +0300

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,31 @@
+import time
+import random
+
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+
+app = Flask(__name__)
+PrometheusMetrics(app)
+
+
+endpoints = ("", "-/healthy", "-/ready")
+
+
+@app.route("/")
+def hello_world():
+    return "Hello world",200
+
+
+@app.route("/-/healthy")
+def healthy():
+    return "ok",200
+
+
+@app.route("/-/ready")
+def ready():
+    return "We are ready", 200
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, threaded=True)
